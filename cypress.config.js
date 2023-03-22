917b20c0f6494d1d97c012b9d819a2ac1cf9671e
Thapelo Musa <89559442+ThapeloMusa@users.noreply.github.com>
2023-03-22 23:25:32 +0200

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
   },
-  
-});
+})
