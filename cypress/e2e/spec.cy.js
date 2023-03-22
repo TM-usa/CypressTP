d835c6a22db6649a6f554cc921c02ba0a312ef4f
Thapelo Musa <89559442+ThapeloMusa@users.noreply.github.com>
2023-03-22 19:08:38 +0200

    commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,27 @@
-import landing from "pages/landing"
+import LandingPage  from "../pages/landing";
+//import "cypress-mochawesome-reporter/register";
 
-// describe('template spec', () => {
-  it('Book', () => {
-    cy.visit('http://hotel-test.equalexperts.io')
-    landing.firstname.
+
+describe('Testing the hotel booking form', () => {
+  const page = new LandingPage();
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    return false;
+  });
+  it('Adding data to form', () => {
+     page.visit();
+     page.fill_form();
+    
   })
-// })
+
+  it('Verifying data', () => {
+    page.visit();
+    cy.wait(3000);
+    page.verify_data();
+  })
+
+  it('Deleting row', () => {
+    page.visit();
+    cy.wait(3000);
+    page.delete_row()
+  })
+})
